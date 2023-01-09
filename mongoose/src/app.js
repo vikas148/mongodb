d33d17538c1c7b14338b7e7e2d9b84defb488fbc
vikas148 <vb334531@gmail.com>
2023-01-09 19:23:44 +0530

    api file

--- mongoose/src/app.js
+++ mongoose/src/app.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 mongoose.set('strictQuery', true);
 //connection creation and creating a new db
 mongoose.connect("mongodb://localhost:27017/iitstu").then(()=> console.log("connection is successful..")).catch((err)=>console.log(err));
@@ -28,7 +29,19 @@ const iitStudentSchema = new mongoose.Schema({
 
     age : {
           type : Number,
+    },
+email:{
+    type:String,
+    required:true,
+    unique:true,
+    validate(value){
+       if(!validator.isEmail(value)){
+        throw new Error("Email is invalid");
+       }
     }
+}
+
+
 
 })
 
@@ -40,106 +53,108 @@ const IitStudent = new mongoose.model("IitStudent",iitStudentSchema)
 //tut 51 create a document or inserting document 
 
 //asynchronous way it take time to save data
-// const Students = new IitStudent ({
-//     name : "Ravi",
+const Students = new IitStudent ({
+    name : "Ravi",
 
-//     stuid : 202001,
-//     branch : "cse",
-//     course : "btech",
-//     active : true
+    stuid : 202001,
+    branch : "cse",
+    course : "btech",
+    active : true
     
-//     // date    : {
-//     //     type :Date,
-//     //     default: Date.now
-//     // }
+    // date    : {
+    //     type :Date,
+    //     default: Date.now
+    // }
     
-// })
+})
 
-// Students.save();
+//Students.save();
 
 
+const createDocument = async()=>{
+
+    try{
+          
+        const Students = new IitStudent ({
+            name : "cnimal",
+        
+            stuid : 202003,
+            branch : "cse",
+            course : "btech",
+            email:  "vb@anand.i",
+            active : true
+            
+            // date    : {
+            //     type :Date,
+            //     default: Date.now
+            // }
+            
+        })
+        
+        const result =await Students.save();
+        console.log(result);
+    }catch(err){
+        console.log(err);
+    }
+
+ }
+
+ createDocument();
+
+//tut 52 inserting many
 // const createDocument = async()=>{
 
 //     try{
           
-//         const Students = new IitStudent ({
+//         const Student1 = new IitStudent ({
 //             name : "Yash",
         
-//             stuid : 202003,
+//             stuid : 202001,
 //             branch : "cse",
 //             course : "btech",
+//             email: "",
+//             date: 22,
 //             active : true
+//             })
+
+//             const Student2 = new IitStudent ({
+//                 name : "sawai",
             
-//             // date    : {
-//             //     type :Date,
-//             //     default: Date.now
-//             // }
-            
-//         })
+//                 stuid : 202002,
+//                 branch : "cse",
+//                 course : "btech",
+//                 age: 33,
+//                 active : true
+//                 })
+
+//                 const Student3 = new IitStudent ({
+//                     name : "sachin",
+                
+//                     stuid : 202003,
+//                     branch : "cse",
+//                     course : "btech",
+//                     age: 26,
+//                     active : true
+//                     })
+                    
+                    
+//                 const Student4 = new IitStudent ({
+//                     name : "siya",
+                
+//                     stuid : 202003,
+//                     branch : "cse",
+//                     course : "btech",
+//                     age: 29,
+//                     active : true
+//                     })
         
-//         const result =await Students.save();
+//         const result =await IitStudent.insertMany([Student1,Student2,Student3]);
 //         console.log(result);
 //     }catch(err){
 //         console.log(err);
 //     }
 
 // }
-
-// createDocument();
-
-//tut 52 inserting many
-const createDocument = async()=>{
-
-    try{
-          
-        const Student1 = new IitStudent ({
-            name : "Yash",
-        
-            stuid : 202001,
-            branch : "cse",
-            course : "btech",
-            date: 22,
-            active : true
-            })
-
-            const Student2 = new IitStudent ({
-                name : "sawai",
-            
-                stuid : 202002,
-                branch : "cse",
-                course : "btech",
-                age: 33,
-                active : true
-                })
-
-                const Student3 = new IitStudent ({
-                    name : "sachin",
-                
-                    stuid : 202003,
-                    branch : "cse",
-                    course : "btech",
-                    age: 26,
-                    active : true
-                    })
-                    
-                    
-                const Student4 = new IitStudent ({
-                    name : "siya",
-                
-                    stuid : 202003,
-                    branch : "cse",
-                    course : "btech",
-                    age: 29,
-                    active : true
-                    })
-        
-        const result =await IitStudent.insertMany([Student1,Student2,Student3]);
-        console.log(result);
-    }catch(err){
-        console.log(err);
-    }
-
-}
 
 //createDocument();
 
@@ -156,18 +171,18 @@ const createDocument = async()=>{
 // getDocument();
 
 
-const getDocument = async()=>{
-    //  const result  =await IitStudent.find({name: "Yash"})//query methods can be used here 
-    //  const result  =await IitStudent.find({branch: {$in : ["cse","civil"]}});
-    //  const result  =await IitStudent.find({$or : [{age:""},{branch:"cse"}]});
-    //  const result  =await IitStudent.find({$or : [{age:""},{branch:"cse"}]}).count();
-    //  const result  =await IitStudent.find({$and : [{branch:"cse"},{name:"Yash"}]});
-     const result  =await IitStudent.find().select({age:1}).sort({age:-1});         //1 or -1;
+// const getDocument = async()=>{
+//     //  const result  =await IitStudent.find({name: "Yash"})//query methods can be used here 
+//     //  const result  =await IitStudent.find({branch: {$in : ["cse","civil"]}});
+//     //  const result  =await IitStudent.find({$or : [{age:""},{branch:"cse"}]});
+//     //  const result  =await IitStudent.find({$or : [{age:""},{branch:"cse"}]}).count();
+//     //  const result  =await IitStudent.find({$and : [{branch:"cse"},{name:"Yash"}]});
+//      const result  =await IitStudent.find().select({age:1}).sort({age:-1});         //1 or -1;
 
-     console.log(result);
-    }
+//      console.log(result);
+//     }
     
-    getDocument();
+    //getDocument();
 
     //tut 57 use try catch
 
@@ -176,19 +191,24 @@ const getDocument = async()=>{
     //     $set:{course:"Btech"}
     //  });    
     // }
-    // //updateDocument("639b53a87f8660f8f408297b");
+    //updateDocument("639b53a87f8660f8f408297b");
 
     //tut 58 delete the document
 
-   const deleteDoument = async(_id)=>{
-    try{
-        const result = await IitStudent.deleteOne({_id} )
-        console.log(result);
-    }catch(err){
-        console.log(err);
-    }
+//    const deleteDoument = async(_id)=>{
+//     try{
+//         const result = await IitStudent.deleteOne({_id} )
+//         console.log(result);
+//     }catch(err){
+//         console.log(err);
+//     }
     
-   }
+//    }
 
 
-    deleteDoument("63a0900f692a75fd9c2d6190")
+  //  deleteDoument("63a0900f692a75fd9c2d6190")
+
+//tut 60 custom validation in mongoose
+
+
+ 
